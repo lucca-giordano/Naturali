0a939367b6a38c86e7821f6b48b368d50b43b46a
Lucca Giordano <giordanolucca871@gmail.com>
2023-11-13 00:18:58 -0300

    start of back end

---
+++ BackEnd/index.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import mysql from 'mysql';
+
+const app = express();
+const port = 8800;
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Lucca_040505',
+    database: 'teste'
+});
+
+db.connect((err) => {
+    if (err) {
+        console.log(err);
+        console.log("Failed to connect to MySQL server!");
+    } else {
+        console.log("Connected to MySQL server!");
+    }
+});
+
+
+app.get("/", (req, res) => {
+    res.json("Hello from the server side!");
+});
+
+app.get("/books", (req, res) => {
+    const q = "SELECT * FROM teste.books;";
+    db.query(q, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send("Internal server error");
+        } else {
+            res.json(data);
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log("Backend server is running!");
+    
+});
